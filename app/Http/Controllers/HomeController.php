06fa63cc500b13c35c3052ff426e097419c6df6d
PutriFR <Iin>
2020-12-01 08:38:21 +0700

    Frontend Dashboard versi 1

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        return view('home');
+    }
+
+    public function hasillatihan()
+    {
+        return view('dashboard.hasillatihan');
+    }
+
+    public function latihansoal()
+    {
+        return view('dashboard.latihansoal');
+    }
+}
