9b490e012d90c899f900500d57c82f22b4de6250
meitavalensina <valensinam@gmail.com>
2020-12-02 08:45:46 +0700

    ..

--- app/Hasiltest.php
+++ app/Hasiltest.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Hasiltest extends Model
 {
-    //
+    protected $table = 'hasiltests';
+    protected $primaryKey = 'no_ujian';
+    protected $fillable = ['id_user', 'nilai_twk','nilai_tkp','nilai_tiu','hasil',];
 }
